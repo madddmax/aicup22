be6b368c4315d51b55203ea52c386734b2e46223
madddmax <madddmax@mail.ru>
2022-07-12 23:47:32 +0300

    Fixed error with debugInterface

--- client-csharp/Strategy/Debug.cs
+++ client-csharp/Strategy/Debug.cs
@@ -5,8 +5,15 @@ namespace AiCup22.Strategy;
 
 public static class Debug
 {
+    private const bool ReleaseMode = true;
+
     public static void DrawLine(DebugInterface debugInterface, Vec2 p1, Vec2 p2)
     {
+        if (ReleaseMode)
+        {
+            return;
+        }
+
         debugInterface.AddPolyLine(new[] {p1, p2}, 0.1, new Color(0, 0, 0, 50));
     }
 }
